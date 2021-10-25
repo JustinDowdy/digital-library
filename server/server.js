1c938bcadc82b1a69a8bd02126ec44b68374eb6a
Justin Dowdy <83055639+JustinDowdy@users.noreply.github.com>
2021-10-24 22:00:21 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,12 +21,7 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-async function startServer() {
-  await server.start()
 server.applyMiddleware({ app })
-}
-
-startServer()
 
 
 app.use(express.urlencoded({ extended: true }));
